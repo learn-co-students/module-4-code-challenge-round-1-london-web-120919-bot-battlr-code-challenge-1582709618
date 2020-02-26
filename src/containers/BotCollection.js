fa10354ac1c72df5f0145547f1e19c5c2d233396
RogeredBacon <jinglis12@googlemail.com>
2020-02-26 10:29:50 +0000

    Checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,22 @@
-import React from "react";
-import BotCard from "../components/BotCard";
+import React from 'react';
+import BotCard from '../components/BotCard';
 
 class BotCollection extends React.Component {
-  //your code here
-
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
-
-};
+	renderBotCards = (bots, addBots) =>
+		bots.map(bot => {
+			return <BotCard bot={bot} moveBots={addBots} key={bot.id} />;
+		});
+	render() {
+		const { bots, addBots } = this.props;
+		return (
+			<div className='ui four column grid'>
+				<div className='row'>
+					{this.renderBotCards(bots, addBots)}
+					Collection of all bots
+				</div>
+			</div>
+		);
+	}
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,61 @@
-import React from "react";
+import React from 'react';
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+	state = {
+		bots: [],
+		myBots: []
+	};
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.getAllBots();
+	}
 
+	getAllBots() {
+		fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+			.then(res => res.json())
+			.then(bots =>
+				this.setState({
+					bots
+				})
+			);
+	}
+
+	addBots = id => {
+		const { myBots } = this.state;
+
+		if (!myBots.includes(id)) {
+			this.setState({
+				myBots: [...myBots, id]
+			});
+		}
+	};
+
+	removeBots = id => {
+		const { myBots } = this.state;
+
+		this.setState({
+			myBots: myBots.filter(bot => bot !== id)
+		});
+	};
+
+	myBotsFind = () => {
+		const { bots, myBots } = this.state;
+
+		return bots.filter(bot => myBots.includes(bot.id));
+	};
+	//start here with your code for step one
+
+	render() {
+		const { bots } = this.state;
+		return (
+			<div>
+				<YourBotArmy removeBots={this.removeBots} myBots={this.myBotsFind()} />
+				<BotCollection bots={bots} addBots={this.addBots} />
+			</div>
+		);
+	}
 }
 
 export default BotsPage;
